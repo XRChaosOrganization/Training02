bfdde19c12ef470a0f1bdec6d92bd07af67bf6a1
Stéphane Lay <stephanelay69@gmail.com>
2022-01-05 15:02:48 +0100

    Spawner 1.0 EDIT
    Spawner crates + raindrops

--- Training_02/Assets/Scripts/SpawnerComponent.cs
+++ Training_02/Assets/Scripts/SpawnerComponent.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class SpawnerComponent : MonoBehaviour
 {
+    public float yOffset;
+    public GameObject cratePrefab;
+    public GameObject rainDropPrefab;
     float waveRainSpawnRate;
+    float waveCrateSpawnRate;
     public float rainSpawnRate;
+    public float crateSpawnRate;
     public List<Transform> player1LandTiles;
     public List<Transform> player2LandTiles;
     public List<Transform> player1OceanTiles;
@@ -13,7 +18,8 @@ public class SpawnerComponent : MonoBehaviour
     
     void Start()
     {
-        waveRainSpawnRate = 6 * rainSpawnRate;
+        waveRainSpawnRate = 2 * rainSpawnRate;
+        waveCrateSpawnRate = 2 * crateSpawnRate;
     }
 
     
@@ -22,13 +28,87 @@ public class SpawnerComponent : MonoBehaviour
         waveRainSpawnRate -= Time.deltaTime;
         if (waveRainSpawnRate < 0)
         {
-
+            StartCoroutine(InstantiateRainDrops());
+            waveRainSpawnRate = 6 * rainSpawnRate;
         }
-        SpawnRainDrops();
+        waveCrateSpawnRate -= Time.deltaTime;
+        if (waveCrateSpawnRate < 0)
+        {
+            StartCoroutine(InstantiateCrates());
+            waveCrateSpawnRate = 6 * crateSpawnRate;
+        }
     }
 
-    public void SpawnRainDrops()
+    IEnumerator InstantiateRainDrops()
     {
-       
+        List<Transform> whereToSpawn = new List<Transform>();
+        List<int> randomizator = new List<int>();
+        while (randomizator.Count < 6)
+        {
+            int rand1 = Random.Range(0, 8);
+            randomizator.Add(rand1);
+        }
+        int R = Random.Range(0, 2);
+        if (R == 0)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                whereToSpawn.Add(player1LandTiles[randomizator[i]]);
+                i++;
+                whereToSpawn.Add(player2LandTiles[randomizator[i]]);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                whereToSpawn.Add(player2LandTiles[randomizator[i]]);
+                i++;
+                whereToSpawn.Add(player1LandTiles[randomizator[i]]);
+            }
+        }
+
+        for (int i = 0; i < 6; i++)
+        {
+            Vector3 pos = new Vector3(whereToSpawn[i].transform.position.x, whereToSpawn[i].transform.position.y + yOffset, whereToSpawn[i].transform.position.z);
+            Instantiate(rainDropPrefab, pos, Quaternion.identity);
+            yield return new WaitForSeconds(rainSpawnRate);
+        }
+    }
+    IEnumerator InstantiateCrates()
+    {
+        List<Transform> whereToSpawn = new List<Transform>();
+        List<int> randomizator = new List<int>();
+        while (randomizator.Count < 6)
+        {
+            int rand1 = Random.Range(0, 12);
+            randomizator.Add(rand1);
+        }
+        int R = Random.Range(0, 2);
+        if (R == 0)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                whereToSpawn.Add(player1OceanTiles[randomizator[i]]);
+                i++;
+                whereToSpawn.Add(player2OceanTiles[randomizator[i]]);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                whereToSpawn.Add(player2OceanTiles[randomizator[i]]);
+                i++;
+                whereToSpawn.Add(player1OceanTiles[randomizator[i]]);
+            }
+        }
+
+        for (int i = 0; i < 6; i++)
+        {
+            Vector3 pos = new Vector3(whereToSpawn[i].transform.position.x, whereToSpawn[i].transform.position.y + yOffset, whereToSpawn[i].transform.position.z);
+            Instantiate(cratePrefab, pos, Quaternion.identity);
+            yield return new WaitForSeconds(crateSpawnRate);
+        }
     }
 }
