843649f5719cf2d63594153d26e2113df86ed852
Vieux <alexlosc@live.fr>
2022-01-07 20:48:21 +0100

    [ART] Mountain snow shader + Shadow drop
    +Add mountain shader for snow
    +Add shadow below dropable

--- Training_02/Assets/Scripts/Components/Buildings/CrateComponent.cs
+++ Training_02/Assets/Scripts/Components/Buildings/CrateComponent.cs
@@ -5,12 +5,36 @@ using UnityEngine;
 public class CrateComponent : MonoBehaviour
 {
     BuildingBehaviour building;
+    public GameObject shadow;
+    public LayerMask hitMask;
+    public float bonusScale;
+    public Vector2 minMaxShadowScale; 
+    private float distanceFromGround;
+    private RaycastHit hit;
+
+    private void Update()
+    {
+        if (shadow == null) return; 
+
+        Ray ray = new Ray(this.transform.position, -this.transform.up);
+        if (Physics.Raycast(ray, out hit, 200.0f, hitMask))
+        {
+            Vector3 point = hit.point;
+            shadow.transform.position = new Vector3(point.x, point.y + 0.1f, point.z);
+            distanceFromGround = Vector3.Distance(point, this.transform.position);
+        }
+
+        float currentScale = Mathf.Clamp((1 / distanceFromGround) * bonusScale, minMaxShadowScale.x, minMaxShadowScale.y);
+        shadow.transform.localScale =  new Vector3(currentScale, 1.0f, currentScale);
+
+        if (distanceFromGround <= 0.5f)
+            Destroy(shadow.gameObject);
+    }
 
     private void Awake()
     {
         building = GetComponentInParent<BuildingBehaviour>(); 
     }
-
 
     private void OnCollisionEnter(Collision col)
     {
--- Training_02/Assets/Scripts/Components/WaterDropComponent.cs
+++ Training_02/Assets/Scripts/Components/WaterDropComponent.cs
@@ -5,6 +5,32 @@ using UnityEngine;
 public class WaterDropComponent : MonoBehaviour
 {
     public int waterQty;
+    public GameObject shadow;
+    public LayerMask hitMask;
+    public float bonusScale;
+    public Vector2 minMaxShadowScale;
+    private float distanceFromGround;
+    private RaycastHit hit;
+
+    private void Update()
+    {
+        if (shadow == null) return;
+
+        Ray ray = new Ray(this.transform.position, -this.transform.up);
+        if (Physics.Raycast(ray, out hit, 200.0f, hitMask))
+        {
+            Vector3 point = hit.point;
+            shadow.transform.position = new Vector3(point.x, point.y + 0.1f, point.z);
+            distanceFromGround = Vector3.Distance(point, this.transform.position);
+        }
+
+        float currentScale = Mathf.Clamp((1 / distanceFromGround) * bonusScale, minMaxShadowScale.x, minMaxShadowScale.y);
+        shadow.transform.localScale = new Vector3(currentScale, 1.0f, currentScale);
+
+        if (distanceFromGround <= 0.5f)
+            Destroy(shadow.gameObject);
+    }
+
     private void OnCollisionEnter(Collision col)
     {
         TileComponent tile = col.gameObject.GetComponent<TileComponent>();
@@ -12,11 +38,7 @@ public class WaterDropComponent : MonoBehaviour
         {
             tile.OnRainDrop(waterQty);
         }
-            
 
         Destroy(this.gameObject);
-            
-
-
     }
 }
