1d6d90ff68e2ef16e94d95b153df5d81120323ea
Vieux <alexlosc@live.fr>
2022-01-08 08:43:39 +0100

    [ART] Main menu play sound + fix fade out issue
    +Add sounds 
    +Set main menu Play sound
    +Fix transition between main menu and main scene

--- Training_02/Assets/Scripts/Components/UIManagerComponent.cs
+++ Training_02/Assets/Scripts/Components/UIManagerComponent.cs
@@ -6,51 +6,69 @@ using TMPro;
 
 public class UIManagerComponent : MonoBehaviour
 {
+    public static UIManagerComponent uIm;
     public Animator transition;
     public GameObject pausePanel;
-    public static UIManagerComponent uIm;
     public TextMeshProUGUI timerDisplayText;
+    public AudioSource mainMenuSource; 
+
     public void Awake()
     {
         uIm = this;
     }
+
     public void Update()
     {
+        if (SceneManager.GetActiveScene().name == "Scene_Title") return; 
+
         float currentTime = GameManager.gm.timer;
         float minutes = Mathf.FloorToInt(currentTime / 60);
         float seconds = Mathf.FloorToInt(currentTime % 60);
         timerDisplayText.text = minutes + ":" + seconds;
-        
     }
+
     public void Play()
     {
-        SceneManager.LoadScene("Scene_Game");
-        StartCoroutine(FadeAnimation());
+        mainMenuSource.Play();
+        StartCoroutine(LoadScene("Scene_Game"));
+        //StartCoroutine(FadeAnimation());
+        //SceneManager.LoadScene("Scene_Game");
     }
+
     public void Quit()
     {
         Application.Quit();
     }
+
     public void Pause()
     {
         Time.timeScale = 0;
         pausePanel.SetActive(true);
     }
+
     public void Resume()
     {
         Time.timeScale = 1;
         pausePanel.SetActive(false);
     }
+
     public void ToMain()
     {
         Time.timeScale = 1;
-        SceneManager.LoadScene("Scene_Title");
-        StartCoroutine(FadeAnimation());
+        StartCoroutine(LoadScene("Scene_Title"));
+        //SceneManager.LoadScene("Scene_Title");
     }
 
-    IEnumerator FadeAnimation()
+    //IEnumerator FadeAnimation()
+    //{
+    //    transition.SetTrigger("StartFadeOut");
+    //    yield return new WaitForSeconds(2.0f);
+    //}
+
+    IEnumerator LoadScene(string _scene)
     {
         transition.SetTrigger("StartFadeOut");
-        yield return new WaitForSeconds(.8f);
+        yield return new WaitForSeconds(2.0f);
+        SceneManager.LoadScene(_scene);
     }
 }
